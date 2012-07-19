eb0f3515d92cc8e9aab23ebb4b8025515c794122
Paul P. Hilscher <pphilscher@gmail.com>
2012-07-19 22:53:05 +0900

    Lagrange interpolation file

---
+++ src/Special/Interpolate/LagrangeInterpolation.h
@@ -0,0 +1,41 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename: LagrangeInterpolation.h
+ *
+ *    Description: One-dimensional Lagrangian interpolation
+ *
+ *         Author: Paul P. Hilscher (2012-), 
+ *
+ *        License: GPLv3+
+ * =====================================================================================
+ */
+
+
+#ifndef __LAGRANGEINTERPOALTION_H
+#define __LAGRANGEINTERPOALTION_H
+
+/**
+ *  @brief Lagrangian interpolation class
+ *
+ *
+ **/
+class LagrangeInterpolation 
+{
+
+
+
+
+  LagrangeInterpolation() {
+
+
+
+  };
+
+
+
+};
+
+
+#endif // __LAGRANGEINTERPOALTION_H
+
