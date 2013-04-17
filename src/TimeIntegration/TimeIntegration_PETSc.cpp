6850acb0207c5dc68f7e6ff8627bfeffdf525254
Paul Hilscher <pphilscher@gmail.com>
2013-04-17 10:55:10 +0900

    cleanup PETSc_TS (time stepper) interface

--- src/TimeIntegration/TimeIntegration_PETSc.cpp
+++ src/TimeIntegration/TimeIntegration_PETSc.cpp
@@ -22,53 +22,53 @@ TimeIntegration_PETSc::TimeIntegration_PETSc(Setup *setup, Grid *grid, Parallel 
                                              TestParticles *particles, Eigenvalue *eigenvalue, Benchmark *bench)
      : TimeIntegration(setup, grid, parallel, vlasov, fields, particles, eigenvalue, bench)
 {
-      PetscInitialize(&setup->argc, &setup->argv, (char *) 0,  help);
+  
+  PetscInitialize(&setup->argc, &setup->argv, (char *) 0,  help);
       
-      // create Matrix Operations
+  // create Matrix Operations
+  int subDiv=0;
+
+  // create Matrix, a shell for Matrix-free methods
+  MatCreateShell(parallel->Comm[DIR_ALL], grid->getLocalSize(), grid->getLocalSize(), grid->getGlobalSize(), grid->getGlobalSize(), &subDiv, &A_F1);
+  MatSetFromOptions(A_F1);
+  MatShellSetOperation(A_F1, MATOP_MULT, (void(*)()) PETScMatrixVector::MatrixVectorProduct);
+  
+  // Initialize implicit solver
+  TSCreate(parallel->Comm[DIR_ALL], &ts);
+  TSSetProblemType(ts, TS_LINEAR);
+  TSSetRHSFunction(ts, PETSC_NULL,TSComputeRHSFunctionLinear, PETSC_NULL);
+  TSSetRHSJacobian(ts, A_F1, A_F1, TSComputeRHSJacobianConstant, PETSC_NULL);
+  TSSetType(ts, TSBEULER);
+  
+  TSSetFromOptions(ts); 
+  // Setup initial vector
+  Vec Vec_init;
+  CComplex *init_x = PETScMatrixVector::getCreateVector(grid, Vec_init);
+  
+  // Set initial condition
+  [=](const CComplex f[NsLD][NmLD][NzLB][NkyLD][NxLB][NvLB]) {
+  
+    int n = 0;
+
+    for(int s = NsLlD; s <= NsLuD; s++) { for(int m   = NmLlD ; m   <= NmLuD ; m++  ) { 
+    for(int z = NzLlD; z <= NzLuD; z++) { for(int y_k = NkyLlD; y_k <= NkyLuD; y_k++) { 
+    for(int x = NxLlD; x <= NxLuD; x++) {  for(int v = NvLlD  ; v <= NvLuD; v++) { 
+
+      init_x[n++] = f[s][m][z][y_k][x][v];
+
+    }}} }}}
       
-      int subDiv=0;
+  }((A6zz) vlasov->f);
 
-      // create Matrix, a shell for Matrix-free methods
-      MatCreateShell(parallel->Comm[DIR_ALL], grid->getLocalSize(), grid->getLocalSize(), grid->getGlobalSize(), grid->getGlobalSize(), &subDiv, &A_F1);
-      MatSetFromOptions(A_F1);
-      MatShellSetOperation(A_F1, MATOP_MULT, (void(*)()) PETScMatrixVector::MatrixVectorProduct);
+  VecRestoreArray(Vec_init, (PetscScalar **) &init_x);
+  TSSetSolution(ts, Vec_init);
       
-      // Initialize implicit solver
-      TSCreate(parallel->Comm[DIR_ALL], &ts);
-      TSSetProblemType(ts, TS_LINEAR);
-      TSSetRHSFunction(ts, PETSC_NULL,TSComputeRHSFunctionLinear, PETSC_NULL);
-      TSSetRHSJacobian(ts, A_F1, A_F1, TSComputeRHSJacobianConstant, PETSC_NULL);
-      TSSetType(ts, TSBEULER);
-
-      TSSetFromOptions(ts); 
-
-      // Setup inital vector
-      Vec Vec_init;
-      CComplex *init_x = PETScMatrixVector::getCreateVector(grid, Vec_init);
-   
-        [=](const CComplex f[NsLD][NmLD][NzLB][NkyLD][NxLB][NvLB]) {
-
-          for(int s = NsLlD; s <= NsLuD; s++) { for(int m   = NmLlD ; m   <= NmLuD ; m++  ) { for(int z = NzLlD; z <= NzLuD; z++) {
-          for(int y_k = NkyLlD; y_k <= NkyLuD; y_k++) { for(int x = NxLlD, n = 0; x <= NxLuD; x++) {  for(int v = NvLlD       ; v <= NvLuD; v++) { 
-
-                init_x[n++] = f[s][m][z][y_k][x][v];
-
-        }}} }}}
-      
-      }((A6zz) vlasov->f);
-
-      VecRestoreArray(Vec_init, (PetscScalar **) &init_x);
-      TSSetSolution(ts, Vec_init);
-      
-      VecDestroy(&Vec_init);
-
-
+  VecDestroy(&Vec_init);
 }
 
 TimeIntegration_PETSc::~TimeIntegration_PETSc()
 {
-        TSDestroy(&ts);
-
+  TSDestroy(&ts);
 }
 
 
@@ -76,45 +76,42 @@ TimeIntegration_PETSc::~TimeIntegration_PETSc()
 double TimeIntegration_PETSc::solveTimeStep(Vlasov *vlasov, Fields *fields, TestParticles *particles, Timing &timing)
 {
     
-    double dt = maxLinearTimeStep;
+  double dt = maxLinearTimeStep;
     
-    PETScMatrixVector pMV(vlasov, fields);
-    TSSetTimeStep(ts, dt);
-    TSStep(ts); 
+  PETScMatrixVector pMV(vlasov, fields);
+  TSSetTimeStep(ts, dt);
+  TSStep(ts); 
     
-    // Get Solution
-    Vec    Vec_F1;   
-    CComplex  *x_F1; 
+  // Get Solution
+  Vec    Vec_F1;   
+  CComplex  *x_F1; 
     
-    TSGetSolution(ts, &Vec_F1);
-    VecGetArray(Vec_F1, (PetscScalar **) &x_F1);
+  TSGetSolution(ts, &Vec_F1);
+  VecGetArray(Vec_F1, (PetscScalar **) &x_F1);
 
+  // copy whole phase space function (waste but starting point) (important due to boundary conditions
+  // we can built wrapper around this and directly pass it
+  [=](CComplex f[NsLD][NmLD][NzLB][NkyLD][NxLB][NvLB]) {
     
-    // copy whole phase space function (waste but starting point) (important due to bounday conditions
-    // we can built wrapper around this and directly pass it
-    [=](CComplex f[NsLD][NmLD][NzLB][NkyLD][NxLB][NvLB]) {
-     
-          for(int s = NsLlD; s <= NsLuD; s++) { for(int m   = NmLlD ; m   <= NmLuD ; m++  ) { for(int z = NzLlD; z <= NzLuD; z++) {
-          for(int y_k = NkyLlD; y_k <= NkyLuD; y_k++) { for(int x = NxLlD, n = 0; x <= NxLuD; x++) {  for(int v = NvLlD       ; v <= NvLuD; v++) { 
+    int n = 0;
+    for(int s = NsLlD; s <= NsLuD; s++) { for(int m   = NmLlD ; m   <= NmLuD ; m++  ) { 
+    for(int z = NzLlD; z <= NzLuD; z++) { for(int y_k = NkyLlD; y_k <= NkyLuD; y_k++) { 
+    for(int x = NxLlD; x <= NxLuD; x++) { for(int v  = NvLlD  ;   v <= NvLuD ; v++  ) { 
 
-               //fs[s][m][z][y_k][x][v] = x_F1[n];
-               f [s][m][z][y_k][x][v] = x_F1[n++];
+      f[s][m][z][y_k][x][v] = x_F1[n++];
 
+    } } }  } } }
       
-     }}} }}}
-      
-    }((A6zz) vlasov->f);
+  }((A6zz) vlasov->f);
            
-    fields->solve(vlasov->f0,  vlasov->f); 
-    VecRestoreArray    (Vec_F1, (PetscScalar **) &x_F1);
+  fields->solve(vlasov->f0,  vlasov->f); 
+  VecRestoreArray    (Vec_F1, (PetscScalar **) &x_F1);
             
-    
-    timing.time += dt;
-    timing.step++;
-    writeTimeStep(timing, maxTiming, dt);
+  timing.time += dt;
+  timing.step++;
+  writeTimeStep(timing, maxTiming, dt);
         
-    return dt;
+  return dt;
 }
-
 
 
